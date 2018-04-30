f55158b7de5df9f8d7e7172418af13dfe45112cb
kdenny <kdenny@deloitte.com>
2018-04-29 21:22:01 -0400

    updated key on Room

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -3,15 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from maintenance.models import *
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Staff)
-admin.site.register(MaintenanceRequest)
-admin.site.register(MaintenanceUpdate)
-admin.site.register(Comment)
-admin.site.register(Room)
-admin.site.register(MaintenanceItem)
-admin.site.register(Issue)
+# from maintenance.models import *
+#
+# # Register your models here.
+#
+# admin.site.register(Category)
+# admin.site.register(Staff)
+# admin.site.register(MaintenanceRequest)
+# admin.site.register(MaintenanceUpdate)
+# admin.site.register(Comment)
+# admin.site.register(Room)
+# admin.site.register(MaintenanceItem)
+# admin.site.register(Issue)
--- maintenance/urls.py
+++ maintenance/urls.py
@@ -5,14 +5,14 @@ from maintenance import views
 ### These API endpoints are illustrated on https://app.swaggerhub.com/apis/Parrot/parrot-fix_api/1.0.0
 
 urlpatterns = [
-    url(r'^maintenance/$', views.NewMaintReqView.as_view()),
-    url(r'^maintenance/rooms/$', views.RoomsListView.as_view()),
-    url(r'^maintenance/rooms/(?P<room_id>[0-9]+)/$', views.MaintenanceItemListView.as_view()),
-    url(r'^maintenance/apartment/(?P<apartment_key>[0-9]+)/$', views.MaintenanceListView.as_view()),
-    url(r'^maintenance/(?P<maint_id>[0-9]+)/$', views.MaintenanceDetailView.as_view()),
-    url(r'^maintenance/(?P<maint_id>[0-9]+)/update/$', views.MaintenanceUpdateView.as_view()),
-    url(r'^maintenance/(?P<maint_id>[0-9]+)/comment/$', views.MaintenanceCommentView.as_view()),
-    url(r'^maintenance/staff/(?P<staff_id>[0-9]+)/$', views.StaffMaintenanceView.as_view()),
-    url(r'^maintenance/category/(?P<category_id>[0-9]+)/$', views.CategoryMaintenanceView.as_view()),
-    url(r'^maintenance/new_issue/$', views.MaintenanceIssue.as_view()),
+    # url(r'^maintenance/$', views.NewMaintReqView.as_view()),
+    # url(r'^maintenance/rooms/$', views.RoomsListView.as_view()),
+    # url(r'^maintenance/rooms/(?P<room_id>[0-9]+)/$', views.MaintenanceItemListView.as_view()),
+    # url(r'^maintenance/apartment/(?P<apartment_key>[0-9]+)/$', views.MaintenanceListView.as_view()),
+    # url(r'^maintenance/(?P<maint_id>[0-9]+)/$', views.MaintenanceDetailView.as_view()),
+    # url(r'^maintenance/(?P<maint_id>[0-9]+)/update/$', views.MaintenanceUpdateView.as_view()),
+    # url(r'^maintenance/(?P<maint_id>[0-9]+)/comment/$', views.MaintenanceCommentView.as_view()),
+    # url(r'^maintenance/staff/(?P<staff_id>[0-9]+)/$', views.StaffMaintenanceView.as_view()),
+    # url(r'^maintenance/category/(?P<category_id>[0-9]+)/$', views.CategoryMaintenanceView.as_view()),
+    # url(r'^maintenance/new_issue/$', views.MaintenanceIssue.as_view()),
 ]
