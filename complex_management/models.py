6d64a6d729966871a80cfa1005be083dac88748b
kdenny <kdenny@deloitte.com>
2018-05-10 00:54:48 -0400

    Built out schema for Maintenance Item app in complex_management

--- complex_management/models.py
+++ complex_management/models.py
@@ -17,12 +17,28 @@ class Property(models.Model):
     def __str__(self):  # __unicode__ for Python 2
         return self.name
 
-class Floorplan(models.Model):
-    name = models.CharField(max_length=250)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='floorplans')
-    rooms = models.ManyToManyField(Room, related_name='floorplans_included_in')
-    floorplan_image = models.CharField(max_length=500)
-    stock_picture = models.CharField(max_length=500)
+
+class RoomItem(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):  # __unicode__ for Python 2
         return self.name
+
+
+class FloorplanRoom(models.Model):
+    name = models.CharField(max_length=100)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='floorplan_rooms')
+    room_items = models.ManyToManyField(RoomItem, related_name='items')
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.property.name + ' - ' + self.name
+
+class Floorplan(models.Model):
+    name = models.CharField(max_length=250)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='floorplans')
+    rooms = models.ManyToManyField(FloorplanRoom, related_name='floorplans_included_in')
+    floorplan_image = models.CharField(max_length=500, blank=True, null=True)
+    stock_picture = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.property.name + ' - ' + self.name
