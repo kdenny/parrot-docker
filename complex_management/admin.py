4dac02bba8b28a88b7a9be6a8f895e12d306cee6
kdenny <kdenny@deloitte.com>
2018-05-10 00:57:22 -0400

    Registered new items in admin

--- complex_management/admin.py
+++ complex_management/admin.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from complex_management.models import Property, Floorplan
+from complex_management.models import Property, Floorplan, FloorplanRoom, RoomItem
 
 # Register your models here.
 
 admin.site.register(Property)
 admin.site.register(Floorplan)
+admin.site.register(FloorplanRoom)
+admin.site.register(RoomItem)
