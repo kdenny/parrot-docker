4d6f41bb4834db2ff285bf2446d93ac3bdd83835
kdenny <kdenny@deloitte.com>
2018-05-13 23:02:10 -0400

    Registered new items in admin

--- complex_management/urls.py
+++ complex_management/urls.py
@@ -3,5 +3,14 @@ from django.conf.urls import url
 from complex_management import views
 
 urlpatterns = [
-    url(r'^login/$', obtain_jwt_token),
+    url(r'^floorplans/$', views.FloorplanView.as_view()),
+    url(r'^floorplans/(?P<floorplan_id>[0-9]+)/$', views.FloorplanView.as_view()),
+    url(r'^properties/$', views.PropertyView.as_view()),
+    url(r'^property/(?P<property_id>[0-9]+)/$', views.PropertyView.as_view()),
+    url(r'^property/(?P<property_id>[0-9]+)/floorplans/', views.FloorplanView.as_view()),
+    url(r'^rooms/(?P<room_id>[0-9]+)/$', views.RoomItemList.as_view()),
+    # url(r'^rooms/(?P<room_id>[0-9]+)/items/(?P<item_id>[0-9]+)', views.ItemIssues.as_view()),
+
+    # url(r'^rooms/(?P<room_id>[\w\-]+)/$', views.RoomItems.as_view()),
+    # url(r'^rooms/(?P<room_id>[\w\-]+)/items/(?P<item_id>[\w\-]+)/$', views.ItemIssues.as_view()),
 ]
--- complex_management/views.py
+++ complex_management/views.py
@@ -11,11 +11,26 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import PropertySerializer, FloorplanSerializer
-from .models import Property, Floorplan
+from .models import Property, Floorplan, FloorplanRoom
 
 # Create your views here.
 
 from rest_framework.response import Response
+
+def add_room_to_floorplan(room, floorplan):
+    f = Floorplan.objects.get(id=floorplan)
+    r = FloorplanRoom.objects.create(
+        name = room['name'],
+        property = f.property
+    )
+    r.save()
+
+    f.rooms.add(r)
+    r.save()
+
+    fs = FloorplanSerializer(f)
+
+    return fs.data
 
 class FloorplanView(APIView):
 
@@ -25,6 +40,32 @@ class FloorplanView(APIView):
             srz = FloorplanSerializer(f, many=True)
 
             return Response(srz.data)
+        else:
+            f = Floorplan.objects.get(id=floorplan)
+            srz = FloorplanSerializer(f)
+
+            return Response(srz.data)
+
+    def post(self, request, floorplan='0'):
+        if (floorplan == '0'):
+            fd = request.data
+            complex = Property.objects.get(id=fd['complex']['id'])
+            f = Floorplan.objects.create(
+                name = fd['name'],
+                property = complex
+            )
+            if 'floorplan_image' in fd:
+                f.floorplan_image = fd['floorplan_image']
+            if 'stock_picture' in fd:
+                f.stock_picture = fd['stock_picture']
+            f.save()
+            srz = FloorplanSerializer(f, many=True)
+
+            return Response(srz.data)
+        else:
+            room_data = request.data
+            data = add_room_to_floorplan(room_data, floorplan)
+            return Response(data)
 
 class PropertyView(APIView):
 
@@ -34,3 +75,21 @@ class PropertyView(APIView):
             srz = PropertySerializer(p, many=True)
 
             return Response(srz.data)
+        else:
+            p = Property.objects.get(id=property)
+            srz = PropertySerializer(p)
+
+            return Response(srz.data)
+
+    def post(self, request):
+        pd = request.data
+        p = Property.objects.create(
+            name = pd['name'],
+            address = pd['address'],
+            city = pd['city']
+        )
+        p.save()
+
+        srz = PropertySerializer(p)
+
+        return Response(srz.data)
